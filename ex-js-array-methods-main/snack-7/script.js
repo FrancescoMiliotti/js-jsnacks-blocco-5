dc6d5945dba50b86c0cdc55799440989969f2a26
francesco.miliotti <francesco.miliotti@email.com>
2025-11-24 22:13:56 +0100

    snack7

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -19,5 +19,13 @@ const students = [
   },
 ];
 
+const studentsId = students.find(persona => {
+
+  return persona.id == 2
+
+  
+})
+console.log(studentsId);
+
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
